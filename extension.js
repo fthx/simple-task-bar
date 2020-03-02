ed8c41a9fa8c301da29d302870c99f5df3976472
fthx <grouik.grouik@yahoo.fr>
2020-03-02 21:08:00 +0100

    Correct external monitor and workspace remove

--- extension.js
+++ extension.js
@@ -7,10 +7,13 @@
 const Lang = imports.lang;
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const Panel = imports.ui.panel;
+//const Panel = imports.ui.panel;
 const Meta = imports.gi.Meta;
 const Shell = imports.gi.Shell;
 const Clutter = imports.gi.Clutter;
+
+const ICON_SIZE = 22;
+const HIDDEN_OPACITY = 127;
 
 
 const WindowList = new Lang.Class({
@@ -21,10 +24,14 @@ const WindowList = new Lang.Class({
 		this.actor = this.apps_menu;
         this._updateMenu();
 		this._restacked = global.display.connect('restacked', Lang.bind(this, this._updateMenu));
+		this._window_change_monitor = global.display.connect('window-left-monitor', Lang.bind(this, this._updateMenu));
+		this._workspace_removed = global.workspace_manager.connect('workspace-removed', Lang.bind(this, this._updateMenu));
 	},
 
 	_destroy: function() {
 		global.display.disconnect(this._restacked);
+		global.display.disconnect(this._window_change_monitor);
+		global.workspace_manager.disconnect(this._workspace_removed);
 		this.apps_menu.destroy();
     },
 
@@ -36,9 +43,43 @@ const WindowList = new Lang.Class({
 
         for ( let workspace_index=0; workspace_index<this.workspaces_count; ++workspace_index ) {
         
-            this.workspace_name = Meta.prefs_get_workspace_name(workspace_index);
+            //this.workspace_name = Meta.prefs_get_workspace_name(workspace_index);
             let metaWorkspace = global.workspace_manager.get_workspace_by_index(workspace_index);
             this.windows = metaWorkspace.list_windows();
+            
+            this.sticky_windows = this.windows.filter(
+            	function(w) {
+                	return !w.is_skip_taskbar() && w.is_on_all_workspaces();
+            	}
+            );
+            
+            this.windows = this.windows.filter(
+            	function(w) {
+                	return !w.is_skip_taskbar() && !w.is_on_all_workspaces();
+               	}
+            );
+            
+            if(this.sticky_windows.length > 0 && workspace_index==0) {
+                for ( let i = 0; i < this.sticky_windows.length; ++i ) {
+                    let metaWindow = this.sticky_windows[i];
+					this.box = new St.Bin({visible: true, 
+        							reactive: true, can_focus: true, track_hover: true});
+					this.box.window = this.sticky_windows[i];
+					this.app = this.tracker.get_window_app(this.box.window);
+                	this.box.connect('button-press-event', Lang.bind(this, function() {
+                							this._activateWindow(metaWorkspace, metaWindow); } ));
+                	this.box.icon = this.app.create_icon_texture(ICON_SIZE);
+                	if (metaWindow.is_hidden()) {
+                		this.box.icon.set_opacity(HIDDEN_OPACITY); this.box.style_class = 'hidden-app';
+                	}
+                	else {
+                	 	if (metaWindow.has_focus()) {this.box.style_class = 'focused-app';}
+                	 	else {this.box.style_class = 'unfocused-app';}
+                	};
+               		this.box.set_child(this.box.icon);
+                	this.apps_menu.add_actor(this.box);
+                }
+            }
             
             if (this.windows.length > 0) {
             	if (global.workspace_manager.get_active_workspace() === metaWorkspace) {
@@ -52,37 +93,6 @@ const WindowList = new Lang.Class({
         			this.workspace_label.set_text((" "+(workspace_index+1)+" ").toString());
         			this.apps_menu.add_actor(this.workspace_label);};
         	};
-                      
-            this.sticky_windows = this.windows.filter(
-            	function(w) {
-                	return !w.is_skip_taskbar() && w.is_on_all_workspaces();
-            	}
-            );
-            this.windows = this.windows.filter(
-            	function(w) {
-                	return !w.is_skip_taskbar() && !w.is_on_all_workspaces();
-               	}
-            );
-
-            if(this.sticky_windows.length && (workspace_index==0)) {
-                for ( let i = 0; i < this.sticky_windows.length; ++i ) {
-                    let metaWindow = this.sticky_windows[i];
-					this.box = new St.Bin({visible: true, 
-        							reactive: true, can_focus: true, track_hover: true});
-					this.box.window = this.sticky_windows[i];
-					this.app = this.tracker.get_window_app(this.box.window);
-                	this.box.connect('button-press-event', Lang.bind(this, function() {
-                							this._activateWindow(metaWorkspace, metaWindow); } ));
-                	this.box.icon = this.app.create_icon_texture(22);
-                	if (metaWindow.is_hidden()) {this.box.icon.set_opacity(127); this.box.style_class = 'hidden-app';}
-                	else {
-                	 	if (metaWindow.has_focus()) {this.box.style_class = 'focused-app';}
-                	 	else {this.box.style_class = 'unfocused-app';}
-                	};
-               		this.box.set_child(this.box.icon);
-                	this.apps_menu.add_actor(this.box);
-                }
-            }
 
             for ( let i = 0; i < this.windows.length; ++i ) {
 	            let metaWindow = this.windows[i];
@@ -92,8 +102,10 @@ const WindowList = new Lang.Class({
 	            this.app = this.tracker.get_window_app(this.box.window);
                 this.box.connect('button-press-event', Lang.bind(this, function() {
                 							this._activateWindow(metaWorkspace, metaWindow); } ));
-                this.box.icon = this.app.create_icon_texture(22);
-                if (metaWindow.is_hidden()) {this.box.icon.set_opacity(127); this.box.style_class = 'hidden-app';}
+                this.box.icon = this.app.create_icon_texture(ICON_SIZE);
+                if (metaWindow.is_hidden()) {
+                	this.box.icon.set_opacity(HIDDEN_OPACITY); this.box.style_class = 'hidden-app';
+                }
                 else {
                 	 if (metaWindow.has_focus()) {this.box.style_class = 'focused-app';}
                 	 else {this.box.style_class = 'unfocused-app';}
