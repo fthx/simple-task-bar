18222934fc00e2a578bbaa9c8fd5d4ca8d0d7824
Francois Thirioux <grouik.grouik@yahoo.fr>
2020-03-07 09:43:44 +0100

    New version.

--- extension.js
+++ extension.js
@@ -7,6 +7,7 @@
 const Lang = imports.lang;
 const St = imports.gi.St;
 const Main = imports.ui.main;
+const Util = imports.misc.util;
 const Meta = imports.gi.Meta;
 const Shell = imports.gi.Shell;
 const Clutter = imports.gi.Clutter;
@@ -26,6 +27,7 @@ const WindowList = new Lang.Class({
 		this._window_change_monitor = global.display.connect('window-left-monitor', Lang.bind(this, this._updateMenu));
 		this._workspace_removed = global.workspace_manager.connect('workspace-removed', Lang.bind(this, this._updateMenu));
 		this._workspace_changed = global.workspace_manager.connect('active-workspace-changed', Lang.bind(this, this._updateMenu));
+		this._workspace_added = global.workspace_manager.connect('workspace-added', Lang.bind(this, this._updateMenu));
 	},
 
 	_destroy: function() {
@@ -33,6 +35,7 @@ const WindowList = new Lang.Class({
 		global.display.disconnect(this._window_change_monitor);
 		global.workspace_manager.disconnect(this._workspace_removed);
 		global.workspace_manager.disconnect(this._workspace_changed);
+		global.workspace_manager.disconnect(this._workspace_added);
 		this.apps_menu.destroy();
     },
 
@@ -40,26 +43,19 @@ const WindowList = new Lang.Class({
     	this.apps_menu.destroy_all_children();
     
         this.tracker = Shell.WindowTracker.get_default();
-        this.workspaces_count = global.workspace_manager.n_workspaces;
+        this.workspaces_count = global.workspace_manager.get_n_workspaces();
 
-        for ( let workspace_index=0; workspace_index<this.workspaces_count; ++workspace_index ) {
+        for ( let workspace_index = 0; workspace_index < this.workspaces_count; ++workspace_index ) {
         
             let metaWorkspace = global.workspace_manager.get_workspace_by_index(workspace_index);
             this.windows = metaWorkspace.list_windows();
             
-            this.sticky_windows = this.windows.filter(
-            	function(w) {
-                	return !w.is_skip_taskbar() && w.is_on_all_workspaces();
-            	}
-            );
-            
-            this.windows = this.windows.filter(
-            	function(w) {
-                	return !w.is_skip_taskbar() && !w.is_on_all_workspaces();
-               	}
-            );
-            
-            if(this.sticky_windows.length > 0 && workspace_index==0) {
+            if (workspace_index==0) {
+            	this.sticky_windows = this.windows.filter(
+            		function(w) {
+                		return !w.is_skip_taskbar() && w.is_on_all_workspaces();
+            		}
+            	);
                 for ( let i = 0; i < this.sticky_windows.length; ++i ) {
                     let metaWindow = this.sticky_windows[i];
 					this.box = new St.Bin({visible: true, 
@@ -79,25 +75,28 @@ const WindowList = new Lang.Class({
                		this.box.set_child(this.box.icon);
                 	this.apps_menu.add_actor(this.box);
                 }
-            }
+            };
             
-            //if (this.windows.length > 0) {
-            	this.box = new St.Bin({visible: true, 
-        						reactive: true, can_focus: true, track_hover: true});
-            	if (global.workspace_manager.get_active_workspace() === metaWorkspace) {
-        			this.box.label = new St.Label({style_class: 'desk-label-active',
-        											y_align: Clutter.ActorAlign.CENTER});
-        		}
-        		else {
-        			this.box.label = new St.Label({style_class: 'desk-label-inactive',
-        											y_align: Clutter.ActorAlign.CENTER});
-        		};
-        		this.box.label.set_text((" "+(workspace_index+1)+" ").toString());
-        		this.box.set_child(this.box.label);
-        		this.apps_menu.add_actor(this.box);
-        		this.box.connect('button-press-event', Lang.bind(this, function() {
-                							this._activateWorkspace(metaWorkspace); } ));
-        	//};
+        	this.ws_box = new St.Bin({visible: true, 
+    						reactive: true, can_focus: true, track_hover: true});
+    		this.ws_box.label = new St.Label({y_align: Clutter.ActorAlign.CENTER});
+        	if (global.workspace_manager.get_active_workspace() === metaWorkspace) {
+    			this.ws_box.label.style_class = 'desk-label-active';
+    		}
+    		else {
+    			this.ws_box.label.style_class = 'desk-label-inactive';
+    		};
+    		this.ws_box.label.set_text((" "+(workspace_index+1)+" ").toString());
+    		this.ws_box.set_child(this.ws_box.label);
+    		this.ws_box.connect('button-press-event', Lang.bind(this, function() {
+            							this._activateWorkspace(metaWorkspace); } ));
+            this.apps_menu.add_actor(this.ws_box);
+        	
+        	this.windows = this.windows.filter(
+            	function(w) {
+                	return !w.is_skip_taskbar() && !w.is_on_all_workspaces();
+               	}
+            );
 
             for ( let i = 0; i < this.windows.length; ++i ) {
 	            let metaWindow = this.windows[i];
@@ -132,20 +131,22 @@ const WindowList = new Lang.Class({
     },
 
     _activateWindow: function(ws, w) {
-        if (global.workspace_manager.get_active_workspace() === ws && w.has_focus()) {
+        if (global.workspace_manager.get_active_workspace() === ws && w.has_focus() 
+        												&& !(Main.overview.visible)) {
        		w.minimize();
        	}
         else {	
-        		w.unminimize();
-				w.unshade(global.get_current_time());
-				w.activate(global.get_current_time());
+        	//w.unminimize();
+			//w.unshade(global.get_current_time());
+			w.activate(global.get_current_time());
 		};
+		Main.overview.hide();
 		if (!(w.is_on_all_workspaces())) { ws.activate(global.get_current_time()); };
     },
 
      _onButtonPress: function(actor, event) {
-        this._updateMenu();
-        this.parent(actor, event);
+     	this._updateMenu();
+     	this.parent(actor, event);
     },
 
 });
