1961f1352268a7589852e03b668824e390226717
fthx <grouik.grouik@yahoo.fr>
2020-03-30 17:24:02 +0200

    Activities label hiding on lock screen too...
    and comment all the code, remove an obsolete function

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 /* 
 	Simple Task Bar
 	Copyright fthx 2020
-	License: GPL v3
+	License GPL v3
 */
 
 const Lang = imports.lang;
@@ -14,54 +14,63 @@ const Clutter = imports.gi.Clutter;
 const AppMenu = Main.panel.statusArea.appMenu;
 const PopupMenu = imports.ui.popupMenu;
 
+// translation needed to restore Places Menu label when disable extension
 const Gettext = imports.gettext.domain('gnome-shell-extensions');
 const _ = Gettext.gettext;
 const N_ = x => x;
 
+// icon size for apps in task bar
 var ICON_SIZE = 22;
+// opacity of inactive or hidden windows (0=min, 255=max)
 var HIDDEN_OPACITY = 127;
 
 
 const WindowList = new Lang.Class({
 	Name: 'WindowList.WindowList',
-
-	_init: function(){	
-		AppMenu._iconBox.hide();
+	
+	// create the task bar container and signals
+	_init: function(){
 	
 		this.apps_menu = new St.BoxLayout({});
 		this.actor = this.apps_menu;
         this._updateMenu();
 		this._restacked = global.display.connect('restacked', Lang.bind(this, this._updateMenu));
 		this._window_change_monitor = global.display.connect('window-left-monitor', Lang.bind(this, this._updateMenu));
-		this._workspace_removed = global.workspace_manager.connect('workspace-removed', Lang.bind(this, this._updateMenu));
 		this._workspace_changed = global.workspace_manager.connect('active-workspace-changed', Lang.bind(this, this._updateMenu));
-		this._workspace_added = global.workspace_manager.connect('workspace-added', Lang.bind(this, this._updateMenu));
 		this._workspace_number_changed = global.workspace_manager.connect('notify::n-workspaces', Lang.bind(this, this._updateMenu));
 	},
-
+	
+	// destroy the task bar
 	_destroy: function() {
+		// disconnect all signals
 		global.display.disconnect(this._restacked);
 		global.display.disconnect(this._window_change_monitor);
-		global.workspace_manager.disconnect(this._workspace_removed);
 		global.workspace_manager.disconnect(this._workspace_changed);
-		global.workspace_manager.disconnect(this._workspace_added);
 		global.workspace_manager.disconnect(this._workspace_number_changed);
+		
+		// destroy task bar container
 		this.apps_menu.destroy();
     },
-
-    _updateMenu: function() {    	
+	
+	// update the task bar
+    _updateMenu: function() {   
+    	// destroy old task bar 	
     	this.apps_menu.destroy_all_children();
     	
+    	// update the focused window title
     	this._updateTitle();
-    
+    	
+    	// track windows and get the number of workspaces
         this.tracker = Shell.WindowTracker.get_default();
         this.workspaces_count = global.workspace_manager.get_n_workspaces();
-
+		
+		// do this for all existing workspaces
         for ( let workspace_index = 0; workspace_index < this.workspaces_count; ++workspace_index ) {
         
             let metaWorkspace = global.workspace_manager.get_workspace_by_index(workspace_index);
             this.windows = metaWorkspace.list_windows();
             
+            // create all sticky windows (means on all workspaces) icons and buttons
             if (workspace_index==0) {
             	this.sticky_windows = this.windows.filter(
             		function(w) {
@@ -93,6 +102,7 @@ const WindowList = new Lang.Class({
             	}
             };
             
+            // create all workspaces labels and buttons
         	this.ws_box = new St.Bin({visible: true, 
     						reactive: true, can_focus: true, track_hover: true});
     		this.ws_box.label = new St.Label({y_align: Clutter.ActorAlign.CENTER});
@@ -113,7 +123,8 @@ const WindowList = new Lang.Class({
                 	return !w.is_skip_taskbar() && !w.is_on_all_workspaces();
                	}
             );
-
+			
+			// create all normal windows icons and buttons
             for ( let i = 0; i < this.windows.length; ++i ) {
 	            let metaWindow = this.windows[i];
 	            let box = new St.Bin({visible: true, 
@@ -140,12 +151,14 @@ const WindowList = new Lang.Class({
         };
     },
     
+    // displays the focused window title
     _updateTitle: function() {
     	if (global.display.get_focus_window()) {
     			AppMenu._label.set_text(global.display.get_focus_window().get_title());
     		};
     },
     
+    // hover on app icon button b shows its window title tt
     _onHover: function(b, tt) {
     	if (b.hover) {
     		AppMenu._label.set_text(tt);
@@ -154,6 +167,7 @@ const WindowList = new Lang.Class({
     	};
     },
     
+    // activate workspace ws
     _activateWorkspace: function(ws) {
 		if (global.workspace_manager.get_active_workspace() === ws) {
 			Main.overview.toggle();
@@ -164,6 +178,7 @@ const WindowList = new Lang.Class({
 		ws.activate(global.get_current_time());
     },
 
+	// switch to workspace ws and activate window w
     _activateWindow: function(ws, w) {
         if (global.workspace_manager.get_active_workspace() === ws && w.has_focus() 
         												&& !(Main.overview.visible)) {
@@ -176,12 +191,7 @@ const WindowList = new Lang.Class({
 		};
 		Main.overview.hide();
 		if (!(w.is_on_all_workspaces())) { ws.activate(global.get_current_time()); };
-    },
-
-     _onButtonPress: function(actor, event) {
-     	this._updateMenu();
-     	this.parent(actor, event);
-    },
+    }
 
 });
 
@@ -191,11 +201,16 @@ function init() {
 }
 
 function enable() {
+	// hide icon before the AppMenu label
+	AppMenu._iconBox.hide();
+	
+	// hide Activities label
 	let activities_indicator = Main.panel.statusArea['activities'];
 	if (activities_indicator) {
-    	activities_indicator.hide();
+    	activities_indicator.container.hide();
 	};
 	
+	// change Places label to folder icon
 	let places_menu_indicator = Main.panel.statusArea['places-menu'];
 	if (places_menu_indicator) {
     	places_menu_indicator.remove_child(places_menu_indicator.get_first_child());
@@ -206,6 +221,7 @@ function enable() {
        	places_menu_indicator.add_actor(places_menu_box);
 	};
     	
+    // activate and display task bar in the panel
 	windowlist = new WindowList;
     let position = 1;
     if ('places-menu' in Main.panel.statusArea)
@@ -215,9 +231,13 @@ function enable() {
 }
 
 function disable() {
+	// destroy task bar
 	windowlist._destroy();
+	
+	// restore default AppMenu label
 	AppMenu._iconBox.show();
 	
+	//display Places label instead of icon
 	let places_menu_indicator = Main.panel.statusArea['places-menu'];
 	if (places_menu_indicator) {
     	places_menu_indicator.remove_child(places_menu_indicator.get_first_child());
@@ -232,8 +252,9 @@ function disable() {
        	places_menu_indicator.add_actor(places_menu_box);
 	};
 	
+	// display Activities label ; take care of locked session to not display Activities label on it
 	let activities_indicator = Main.panel.statusArea['activities'];
-	if (activities_indicator) {
-    	activities_indicator.show();
+	if (activities_indicator && !Main.sessionMode.isLocked) {
+    	activities_indicator.container.show();
     };
 }
